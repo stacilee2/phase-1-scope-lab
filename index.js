5943bb2aba13e26eebac15ee0123a8022d6ac82f
Staci Rosales <stacileep2@gmail.com>
2022-09-26 14:08:41 -0500

    commit tree d08f109c1e3786559ce576ec9ae7caf0b134b847

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Suzy';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'bob';
+}
